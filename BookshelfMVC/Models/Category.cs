5375dc44af6589489b90d3aaa410edff0d529da1
etwilsongit <etwilson.2012@gmail.com>
2017-05-03 14:53:45 -0400

    initial commit

---
+++ BookshelfMVC/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BookshelfMVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
